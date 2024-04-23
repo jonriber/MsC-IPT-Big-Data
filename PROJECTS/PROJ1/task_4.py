0df1e8f078b1d6f0f2850d81dce3846b6fa3045d
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-04-23 22:02:15 +0100

    job schedule configuration

--- PROJECTS/PROJ1/task_4.py
+++ PROJECTS/PROJ1/task_4.py
@@ -15,6 +15,18 @@ def retrieve_data_from_api():
     except Exception as e:
         logging.error(f'An error occurred: {str(e)}')
         
+def job():
+    retrieve_data_from_api()
+
+
+        
 if __name__ == "__main__":
     
     # retrieve_data_from_api()
+    # Schedule the job every hour
+    schedule.every().hour.do(job)
+
+    # Run the schedule for 5 days
+    for _ in range(24 * 5):
+        schedule.run_pending()
+        time.sleep(3600)  # Sleep for 1 hour
