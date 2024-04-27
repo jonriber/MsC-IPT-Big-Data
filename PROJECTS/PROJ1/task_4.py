9a52c0dfe1d15018d3c2d0134e610809dab6aec4
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-04-27 10:59:48 +0100

    task 4 basic structure

--- PROJECTS/PROJ1/task_4.py
+++ PROJECTS/PROJ1/task_4.py
@@ -9,24 +9,41 @@ logging.basicConfig(filename='api_data.log', level=logging.INFO, format='%(ascti
 def retrieve_data_from_api():
     try:
         # API data retrieval code here
+        url = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random"
+
+        headers = {
+            "accept": "application/json",
+            "X-RapidAPI-Key": "aa24d1a197msh7c322f1111480c2p16c025jsn35b22e6582d6",
+            "X-RapidAPI-Host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com"
+        }
+
+        response = requests.get(url, headers=headers)
+
+        print(response.json())
         
         
-        logging.info('Data retrieved successfully from the API')
+        response = requests.get(url, headers=headers)
+
+        logging.info(f'Data retrieved successfully from the API: {response.json()}')
     except Exception as e:
+        print(f'An error occurred: {str(e)}')
         logging.error(f'An error occurred: {str(e)}')
         
 def job():
+    print("Job started")
     retrieve_data_from_api()
 
 
         
-if __name__ == "__main__":
+# if __name__ == "__main__":
     
-    # retrieve_data_from_api()
-    # Schedule the job every hour
-    schedule.every().hour.do(job)
+# retrieve_data_from_api()
+# Schedule the job every hour
+# schedule.every().hour.do(job)
 
-    # Run the schedule for 5 days
-    for _ in range(24 * 5):
-        schedule.run_pending()
-        time.sleep(3600)  # Sleep for 1 hour
+# Run the schedule for 5 days
+for _ in range(24 * 5):
+    print("Running the schedule",_)
+    job()
+    # schedule.run_pending()
+    time.sleep(60)  # Sleep for 1 hour
