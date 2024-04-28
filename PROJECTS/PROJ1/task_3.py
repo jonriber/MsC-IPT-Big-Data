7863ba6ff72f0e43705509e9804b5fa1680dce4c
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-04-28 18:17:26 +0100

    next events extraction for european parlment

--- PROJECTS/PROJ1/task_3.py
+++ PROJECTS/PROJ1/task_3.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
+TARGET_URL = "https://www.europarl.europa.eu/news/pt"
 
 def quit_driver():
     driver.quit()
@@ -16,9 +16,43 @@ def get_last_news(id,driver):
         print("An error occurred:", e)
         return None
 
-def get_next_events(id,driver):
-    # Get the next events
-    pass
+def get_next_events_list(id,driver):
+    
+    try:
+        event_list_element = driver.find_element(By.ID, id)
+        list_items = event_list_element.find_elements(By.TAG_NAME, "li")
+
+        return list_items
+    except Exception as e:
+        print("No list elements found:", e)
+        return None
+    
+def get_event_context(event):
+    try:
+        date_element = event.find_element(By.CLASS_NAME, "ep_date")
+        date = date_element.text.strip()
+        
+        type_element = event.find_element(By.CLASS_NAME, "ep_type")
+        event_type = type_element.text.strip()
+        
+        # Extract the event title
+        title_element = event.find_element(By.CLASS_NAME, "ep_title")
+        title = title_element.text.strip()
+        
+        # Extract the event location
+        location_element = event.find_element(By.CLASS_NAME, "ep_location")
+        location = location_element.text.strip()
+    
+     # Print the extracted information for each event
+        print("Date:", date)
+        print("Type:", event_type)
+        print("Title:", title)
+        print("Location:", location)
+        print()  # Add a line break between events
+        return {date, event_type, title, location}
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
 
 def get_news_context(news):
     
@@ -52,7 +86,8 @@ def init_driver():
 
 if __name__ == "__main__":
     driver = init_driver()
-    driver.get("https://www.europarl.europa.eu/news/pt")
+    
+    driver.get(TARGET_URL)
 
     extracted_news = get_last_news("ep_gridcolumn",driver)
     # if len(extracted_news) >5:
@@ -65,5 +100,15 @@ if __name__ == "__main__":
             final_news.append(new)
             # print("New:", new)
     print("Final news:", final_news)
-
+    
+    final_event_list = []
+    extracted_next_events = get_next_events_list("event-list",driver)
+    
+    for event in extracted_next_events:
+        event = get_event_context(event)
+        if event != None:
+            final_event_list.append(event)
+            # print("Event:", event)
+    print("Final event list:", final_event_list)
+    
     quit_driver()
